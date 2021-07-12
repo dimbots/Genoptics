23c1f5f382abb73d945b528a8229c9f9cf498c92
Dimbo <dim.botskaris@gmail.com>
2021-07-12 11:33:42 +0300

    add

--- data/qc_metrics.v2.sh
+++ data/qc_metrics.v2.sh
@@ -2,26 +2,26 @@
 # QC Metrics (Deeptools) + Normalization
 
 #	SET DIR QC_METRICS
-#	mkdir qc_metrics
-#	cd qc_metrics/
+	mkdir qc_metrics
+	cd qc_metrics/
 
-#ln -s ../mapping/*.bam .
-#ln -s ../mapping/*.bam.bai .
+ln -s ../mapping/*.bam .
+ln -s ../mapping/*.bam.bai .
 
-#	echo "RENAME FILES FROM 0869... TO CONDITIONS (e.g Set8KO_A_input.) IN DIRECTORY (qc_metrics). WHEN COMPLETE TYPE [done]."
-#	read response
+	echo "RENAME FILES FROM 0869... TO CONDITIONS (e.g Set8KO_A_input.) IN DIRECTORY (qc_metrics). WHEN COMPLETE TYPE [done]."
+	read response
 
 # CREATE MULTIBAM FILE
-#multiBamSummary bins --bamfiles *.bam -p 7 -o multiBam.npz
+multiBamSummary bins --bamfiles *.bam -p 7 -o multiBam.npz
 
 # PLOT HEATMAP
-#plotCorrelation -in multiBam.npz --corMethod spearman --colorMap Blues --plotHeight 11.5 --plotWidth 13  --whatToPlot heatmap --plotNumbers -o SpearmanCor_readCounts.png
+plotCorrelation -in multiBam.npz --corMethod spearman --colorMap Blues --plotHeight 11.5 --plotWidth 13  --whatToPlot heatmap --plotNumbers -o SpearmanCor_readCounts.png
 
 # PLOT READ COVERAGE
-#plotCoverage --bamfiles *.bam --skipZeros -p 7 --verbose -o Coverage_plot.png
+plotCoverage --bamfiles *.bam --skipZeros -p 7 --verbose -o Coverage_plot.png
 
 # PLOT FINGERPRINT
-#plotFingerprint -b *.bam -p 7 -plot finger_print_all.png
+plotFingerprint -b *.bam -p 7 -plot finger_print_all.png
 
 #########################################################################################
 
@@ -90,47 +90,47 @@ mv *.bw normalization/
 # 			COMPUTEMATRIX
 
 #	SET DIR NORMALIZATION
-#	cd normalization/
+	cd normalization/
 
-#	echo "          START COMPUTEMATRIX            "
+	echo "          START COMPUTEMATRIX            "
 
-#	echo "-----------------------------------------"
-#	echo "   TYPE BIGWIG FILES (TREATMENT-INPUT)"
-#	echo "-----------------------------------------"
+	echo "-----------------------------------------"
+	echo "   TYPE BIGWIG FILES (TREATMENT-INPUT)"
+	echo "-----------------------------------------"
 
-#	echo "TYPE TREATMENT BW FILE. ELSE TYPE [none]"
-#	read bw1
-#	echo "TYPE INPUT BW FILE. ELSE TYPE [none]"
-#	read bw2
-#	echo "TYPE TREATMENT BW FILE. ELSE TYPE [none]"
-#	read bw3
-#	echo "TYPE INPUT BW FILE. ELSE TYPE [none]"
-#	read bw4
-#	echo "TYPE OUT MATRIX FILE. E.G (matrix.0865_0866)"
-#	read out_matrix
-#	echo "TYPE OUT REGIONG FILE. E.G (out_regions.0865_0866)"
-#	read out_regions
+	echo "TYPE TREATMENT BW FILE. ELSE TYPE [none]"
+	read bw1
+	echo "TYPE INPUT BW FILE. ELSE TYPE [none]"
+	read bw2
+	echo "TYPE TREATMENT BW FILE. ELSE TYPE [none]"
+	read bw3
+	echo "TYPE INPUT BW FILE. ELSE TYPE [none]"
+	read bw4
+	echo "TYPE OUT MATRIX FILE. E.G (matrix.0865_0866)"
+	read out_matrix
+	echo "TYPE OUT REGIONG FILE. E.G (out_regions.0865_0866)"
+	read out_regions
 
-#	echo "Set path to genes.bed file"
-#	read genes
+	echo "Set path to genes.bed file"
+	read genes
 
 
-#	if [[ "$bw3" != "none" && "bw4" != "none" ]]
-#		then
-#		echo "RUNNING WITH 4 SAMPLES"
-#		computeMatrix reference-point -b 5000 -a 5000 -R $genes -S $bw1 $bw2 $bw3 $bw4 --skipZeros -o $out_matrix --outFileSortedRegions $out_regions -p 7
+	if [[ "$bw3" != "none" && "bw4" != "none" ]]
+		then
+		echo "RUNNING WITH 4 SAMPLES"
+		computeMatrix reference-point -b 5000 -a 5000 -R $genes -S $bw1 $bw2 $bw3 $bw4 --skipZeros -o $out_matrix --outFileSortedRegions $out_regions -p 7
 
-#		out_plot="Heatmap_$out_matrix"
-#		plotHeatmap -m $out_matrix -out $out_plot --colorMap Blues
-#	else
-#		echo "RUNNING WITH 2 SAMPLES"
-#		computeMatrix reference-point -b 5000 -a 5000 -R $genes -S $bw1 $bw2 --skipZeros -o $out_matrix --outFileSortedRegions $out_regions -p 7
+		out_plot="Heatmap_$out_matrix"
+		plotHeatmap -m $out_matrix -out $out_plot --colorMap Blues
+	else
+		echo "RUNNING WITH 2 SAMPLES"
+		computeMatrix reference-point -b 5000 -a 5000 -R $genes -S $bw1 $bw2 --skipZeros -o $out_matrix --outFileSortedRegions $out_regions -p 7
 
-#		out_plot="Heatmap_$out_matrix"
-#		plotHeatmap -m $out_matrix -out $out_plot --colorMap Blues
-#	fi
+		out_plot="Heatmap_$out_matrix"
+		plotHeatmap -m $out_matrix -out $out_plot --colorMap Blues
+	fi
 
-#mv $out_plot ../
+mv $out_plot ../
 
 
 
